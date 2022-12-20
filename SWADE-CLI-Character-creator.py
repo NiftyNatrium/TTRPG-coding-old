5b3203b5f2692375ac9540b9682bad7211b80abf
Your Name <rain>
2022-12-19 18:49:31 -0600

    Changed some formatting for prints and cleaned up some code

--- SWADE-CLI-Character-creator.py
+++ SWADE-CLI-Character-creator.py
@@ -4,7 +4,7 @@
 attributePoints=5 #points to spend on attributes
 skillPoints=12 #how many points to spend on skills
 hindrancePoints=0 #determines how much extra points for other catergories, dependant on how many hindrances chosen, max of 4.
-edgePoints=1 #normally 0 but assumed to be a human will change later maybe
+BedgePoints=1 #normally 0 but assumed to be a human will change later maybe
 
 attributes= { #these are the attributes which allow derived traits, determine skills, and enable certain edges to be taken
   "Agility" : 4,
@@ -21,32 +21,39 @@ skills= { #these are the skills
   "Driving" : 2,
   "Performance" : 2,
   "Persuasion" : 4
-  #need to add the rest when I have the book  
+  #need to add the rest when I have the book
 }
 
 #naming character
 print("Welcome to your Character Creation!")
-name=input("Please name your character:") #sets the name, eventually should write this to a file
-decision=input("Where would you like to start? Attributes, Skills, Hindrances, Edges, or Inventory?")
+name=input("Please name your character: ") #sets the name, eventually should write this to a file
 
-#here we want a switch function to determine the choice made at the start 
+print("\nAttributes, Skills, Hindrances, Edges, or Inventory")
+decision=input("Where would you like to start: ")
+
+#here we want a switch function to determine the choice made at the start
 match decision:
   case "Attributes": #attribute raises here
     while attributePoints>0: #loops until attributes are spent
-      print("Choose to increase: Agility, Smarts. Spirit, Strength, or Vigor.")
-      if attributes[userInput] + 2 > 12: #max quantity for attribute
-        print("Pick a different attribute!")
-      else: # the normal case
-        attributes[userInput] += 2 #increases the desired stat
-        attributePoints -= 1 #decreases the while loop variable
-    print("Your attributes are:\n\
-          \tAgility: d",attribute[Agility],"\n\
-          \tSmarts: d",attribute[Smarts],"\n\
-          \tSpirit: d",attribute[Spirit],"\n\
-          \tStrength: d",attribute[Strength],"\n\
-          \tVigor: d",attribute[Vigor],sep="") #prints the attributes, sep fixes the spacing.
-        
-  
 
+      # Get user descision on what to increase
+      print() # Just give us some visual separation for each iteration
+      # Do a confusing looking print statement for current allocations LoL
+      print(', '.join([f"{key}: {attributes[key]}" for key in attributes]))
+ 	  # Show how many points are left to spend
+      print(f"You have {attributePoints} point{'' if attributePoints == 1 else 's'} left to spend")
+      userAttributeInc = input("Choose to increase: ")
+
+      # Ensure that the selected increase is a valid selection
+      if userAttributeInc in attributes and attributes[userAttributeInc] + 2 <= 12:
+        attributes[userAttributeInc] += 2 #increases the desired stat
+        attributePoints -= 1 #decreases the while loop variable
+      else:
+        print("Pick a different attribute!")
+
+    # Print all Athe attributes stored in the dict
+    print("Your atttributes are:")
+    for key in attributes:
+      print(f'\t{key}: {attributes[key]}')
 
 
